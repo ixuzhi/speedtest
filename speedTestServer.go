f03b9f240630faafa8fffd5faa32b364bed55911
ixuzhi <ixuzhi100@gmail.com>
2019-08-09 00:44:42 +0800

    speedtestserver list config.

--- speedTestServer.go
+++ speedTestServer.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/xml"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -8,14 +9,48 @@ import (
 	"time"
 )
 
+/*
+<?xml version="1.0" encoding="UTF-8"?>
+<settings>
+<servers>
+<server url="http://speedtest.oppinord.no:8080/speedtest/upload.php" lat="70.6632" lon="23.6817" \
+name="Hammerfest" country="Norway" cc="NO" sponsor="Hammerfest Energi Bredbånd AS" id="21239" \
+host="speedtest.oppinord.no:8080" />
+</servers>
+</settings>
+*/
+
+//https://github.com/sivel/go-speedtest/blob/master/speedtest.go#L301
+type ServerList struct {
+	Servers []ServerInfo `xml:"servers>server"`
+}
+
+type ServerInfo struct {
+	Url     string `xml:"url,attr"`
+	Lat     string `xml:"lat,attr"`
+	Lon     string `xml:"lon,attr"`
+	Name    string `xml:"name,attr"`
+	Country string `xml:"country,attr"`
+	Cc      string `xml:"cc,attr"`
+	Sponsor string `xml:"sponsor,attr"`
+	Id      string `xml:"id,attr"`
+	HostUrl string `xml:"host,attr"`
+}
+
 func IsExist(f string) bool {
 	_, err := os.Stat(f)
 	return err == nil || os.IsExist(err)
 }
 func main() {
-	var FileName string = "server.dat"
+	var FileName string = "servers.dat"
+	var data []byte
+	var err error
 	if IsExist(FileName) {
 		fmt.Printf("%s exist\n", FileName)
+		data, err = ioutil.ReadFile(FileName)
+		if err != nil {
+			fmt.Println(err)
+		}
 	} else {
 		fmt.Println("not exist")
 		ipUrl := "https://www.speedtest.net/speedtest-servers.php"
@@ -42,6 +77,14 @@ func main() {
 			ioutil.WriteFile("server.dat", data, 0755)
 			fmt.Println("ok")
 		}
+	}
+	var ServersInfo ServerList
+	//ServersInfo.Servers
+	if err := xml.Unmarshal(data, &ServersInfo); err != nil {
+		fmt.Println(err)
+		return
+	} else {
+		fmt.Println(len(ServersInfo.Servers))
 	}
 
 }
