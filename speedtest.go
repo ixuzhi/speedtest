18dca0e053a34dfc690ca95e58bcd28d07532ef9
ixuzhi <ixuzhi100@gmail.com>
2019-08-10 01:04:36 +0800

    speedtest.go get ClientInfo.

--- speedtest.go
+++ speedtest.go
@@ -2,6 +2,24 @@ package main
 
 import "fmt"
 
-func main(){
+type ClientInfo struct {
+	ClientIP  string
+	ClientLat float64
+	ClientLon float64
+}
+
+func getIPAndLatLon() (client ClientInfo, err error) {
+	var clientInfo ClientInfo
+	return clientInfo, nil
+}
+
+func main() {
+	var clientInfo ClientInfo
 	fmt.Println("speedtest init.")
+	clientInfo, err := getIPAndLatLon()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("%+v\n", clientInfo)
 }
